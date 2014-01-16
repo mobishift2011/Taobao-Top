e5647511ec263a9ef0eb3732d979a8288f6f32a4
ethan <mobishift@gmail.com>
2014-01-16 14:53:03 +0800

    settings

--- baokuan/urls.py
+++ baokuan/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 
-urlpatterns = patterns('',
+urlpatterns = patterns('baokuan',
     url(r'^api/', include('apis.urls')),
     url(r'^admin/', include('admins.urls')),
     url(r'^share/', include('shares.urls'))
