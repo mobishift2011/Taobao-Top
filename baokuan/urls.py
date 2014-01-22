f0f68df48a92f775f91626ac000cb70f00951bae
ethan <mobishift@gmail.com>
2014-01-22 18:48:05 +0800

    url fix

--- baokuan/urls.py
+++ baokuan/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     url(r'^(baokuan/)?api/', include('apis.urls')),
     url(r'^(baokuan/)?admin/', include('admins.urls')),
-    url(r'^(baokaun/)?share/product/(?P<product_id>\w+)/$', 'shares.views.productHandle')
+    url(r'^(baokuan/)?share/', include('shares.urls'))
 )
