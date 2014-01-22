d2e3064a2a60f374ab6273758bc35e8697eb5f6b
ethan <mobishift@gmail.com>
2014-01-22 17:51:05 +0800

    fix bug of url for share

--- baokuan/shares/urls.py
+++ baokuan/shares/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('shares.views',
-    url(r'^product/(?P<product_id>\w+)/$', 'productHandle'),
+    url(r'^(baokuan/)?product/(?P<product_id>\w+)/$', 'productHandle'),
 )
