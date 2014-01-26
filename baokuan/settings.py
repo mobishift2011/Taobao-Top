9d3813d5b5d3b56dda4b707ae41dddae5c6901a9
ethan <mobishift@gmail.com>
2014-01-26 11:56:11 +0800

    fix error url of reset password

--- baokuan/settings.py
+++ baokuan/settings.py
@@ -179,7 +179,7 @@ CELERYBEAT_SCHEDULE = {
     },
     'paper_online': {
         'task': 'cron.tasks.paper_online',
-        'schedule': crontab(minute=48, hour=11)
+        'schedule': crontab(minute=57, hour=11)
     },
     'lottery_online': {
         'task': 'cron.tasks.lottery_online',
