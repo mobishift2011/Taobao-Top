3b97ac1b3a69c358a3616687106ad4723940804d
ethan <mobishift@gmail.com>
2014-03-20 16:30:44 +0800

    page 15

--- baokuan/admins/admin.py
+++ baokuan/admins/admin.py
@@ -77,7 +77,7 @@ def usersHandle(request):
 
 @http_basic_auth
 def papersHandle(request):
-    papers = Paper.objects().order_by('-period')[0:50]
+    papers = Paper.objects().order_by('-period')[0:15]
     return render(request, 'papers.html', {'papers': papers})
 
 
